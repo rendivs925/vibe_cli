bbfabf2b798cb18de467d4dbf6177a6625727e8c
rendivs925 <hardleberg@gmail.com>
2026-01-28 19:33:20 +0800

    removed legacy code

--- src/clipboard.rs
+++
@@ -1,9 +0,0 @@
-use anyhow::{Context, Result};
-
-pub fn copy_to_clipboard(text: &str) -> Result<()> {
-    let mut clipboard = arboard::Clipboard::new().context("Failed to access system clipboard")?;
-    clipboard
-        .set_text(text.to_string())
-        .context("Failed to set clipboard text")?;
-    Ok(())
-}
